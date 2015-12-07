c64ebf26176acc62d78db8f1962015191fa19bfe
rickyjanuari <ricky010193@gmail.com>
2015-12-07 14:31:16 +0700

    Added Post CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	def index
+
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def edit
+		
+	end
+
+	
+
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Update succes"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Save Succes"
+		else
+			render 'new', notice: "oh no, Ricky Failed Save"
+		end
+	end
+
+
+
+	def show
+		
+	end
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+end
