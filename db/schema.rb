8b54125781df68b29df404e44c33521f71f2a725
rickyjanuari <ricky010193@gmail.com>
2015-12-10 06:14:53 +0700

    Setup friendly_id for projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209083634) do
+ActiveRecord::Schema.define(version: 20151209230923) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 20151209083634) do
     t.string   "link"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
+
+  add_index "projects", ["slug"], name: "index_projects_on_slug", unique: true
 
 end
