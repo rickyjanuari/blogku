d43efce89ab3fb764dc8b356dde82d5ac386da83
rickyjanuari <ricky010193@gmail.com>
2015-12-10 06:06:11 +0700

    Setup & styled basic project functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209075435) do
+ActiveRecord::Schema.define(version: 20151209083634) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -28,5 +28,13 @@ ActiveRecord::Schema.define(version: 20151209075435) do
 
 # Could not dump table "posts" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
